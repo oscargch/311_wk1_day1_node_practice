bc999c9fdf194023aff3337004431a8cb383a073
Oscar Garcia <oscar.gch@gmail.com>
2020-01-08 18:34:21 -0600

    finished node practice

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,19 +1,31 @@
 // import moment here; use this package in each function
+const moment = require("moment");
+// console.log(moment());
 
+
+//'Return today\'s date in the format: May 29, 2019'
 const today = () => {
   // write code for dates.today
+return moment().format("dddd");
+// return Moment().format('LL');
 
 }
+
+console.log(today());
 
 const calendar = () => {
   // write code for dates.calendar
-
+  return moment().format("MMM DD, YYYY");
 }
+console.log(calendar());
 
+//03:21:51 PM
 const currentTime = () => {
   // write code for dates.currentTime
-
+  return moment().format("HH:MM:SS A");
 }
+console.log(currentTime());
+
 
 module.exports = {
   today,
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,24 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num%2 == 0)
+  return true;
+else
+  return false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  console.log(arr.reduce((a, b) => a + b, 0))
+  return arr.reduce((a, b) => a + b, 0)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+return arr.reduce((a, b) => a + b === sum);
 
 }
+
+
 
 module.exports = {
   isEven,
